3b8945ddddc01beba93d235cb8f73cb4f6d4744c
Hong Seok Joo <97148398+BambooStreet@users.noreply.github.com>
2024-03-02 00:31:07 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+
+# from dotenv import load_dotenv
+# load_dotenv()
+
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+import time
+
+#from langchain_openai import ChatOpenAI
+#llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+output_parser = StrOutputParser()
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+chain = llm | output_parser
+
+
+st.title('시를 써보자 :sunglasses:')
+#content = '코드'
+#result = chain.invoke(f"{content}에 대한 시를 써줘")
+#print(result)
+#print(type(result))
+
+content = st.text_input('시의 주제를 제시해주세요.')
+
+st.write(f'시의 주제는 {content}입니다.')
+
+if st.button('시 작성 요청하기'):
+    with st.spinner('Wait for it...'):
+        time.sleep(5)
+        result = chain.invoke(f"{content}에 대한 시를 써줘")
+        st.write(result)
